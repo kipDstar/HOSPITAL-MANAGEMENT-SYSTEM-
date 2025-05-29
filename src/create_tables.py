b8dfe05998e4e892eadea34f73a8896811b878bc
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-05-29 20:28:25 +0300

    Remove create_tables.py script and update database files

--- src/create_tables.py
+++
@@ -1,8 +0,0 @@
-# src/create_tables.py
-
-from src.database import engine
-from src.models import Base
-
-print("Creating all tables in the database...")
-Base.metadata.create_all(bind=engine)
-print("All tables created successfully.")
